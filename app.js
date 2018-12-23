2063e681dc04c6c7b3ad8b0722a2993b92252da7
bnguser <batngoal@gmail.com>
2018-12-23 22:58:46 +0530

    Routing

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = express();
-const message = require('./message.js');
-let all_letters = '';
+// const message = require('./message.js');
+// let all_letters = '';
 
-console.log(message["letters"]);
+// console.log(message["letters"]);
 
 
 app.get("/",function(request,response){
-	for(i=0; i<= message["letters"].length -1 ; i++){
-		all_letters += message["letters"][i] + "<br/>";
-	}
-	response.send(all_letters);
+	// for(i=0; i<= message["letters"].length -1 ; i++){
+	// 	all_letters += message["letters"][i] + "<br/>";
+	// }
+	response.send("Hello world");
+
+})
+
+app.get("/users/:name",function(req,res){
+	res.send(req["params"]["name"]);
 })
 
 
