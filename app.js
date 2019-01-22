45fdb3366c440114d65881f78e98e38944081173
bnguser <batngoal@gmail.com>
2019-01-22 23:22:23 +0530

    reading data

--- app.js
+++ app.js
@@ -27,7 +27,10 @@ app.listen(3000,function(error){
 	}
 })*/
 const http = require('http');
+const fs = require('file-system');
 
-http.createServer(function(request, response){
-
+http.createServer(function(req, res){
+	let file = fs.readFileSync('.'+req['url'] + '.txt','utf8');
+	res.write(file);
+res.end();
 }).listen(3000);
